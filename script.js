00c57c7a8dd457332998b39d5b2a823a96341c02
DikzNakano <dikzpreset11@gmail.com>
2026-05-31 18:09:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,13 +9,17 @@ const SUPABASE_URL =
 const SUPABASE_KEY =
 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndncmVkemllcHNubHFlcm9yeHp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAyMDU5MDIsImV4cCI6MjA5NTc4MTkwMn0.w9yjTQyxtfCA0zpw8ngsr1PdiGsOYU9KDLbPjtfeVQg";
 
+let supabase;
+
 try {
 
-    const supabase =
+    supabase =
     window.supabase.createClient(
         SUPABASE_URL,
         SUPABASE_KEY
-    ); 
+    );
+
+    alert("client oke");
 
 } catch(err){
 
